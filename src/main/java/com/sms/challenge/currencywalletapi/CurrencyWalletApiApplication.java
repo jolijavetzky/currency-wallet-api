a7fc36138efbf83f9aab386835fada897a061126
jolijavetzky <riverplate>
2021-06-04 15:28:07 -0300

    initial commit

---
+++ src/main/java/com/sms/challenge/currencywalletapi/CurrencyWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.sms.challenge.currencywalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyWalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyWalletApiApplication.class, args);
+	}
+
+}
