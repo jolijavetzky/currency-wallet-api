dda1e984f34ed53dd2f6bdd97527cbb15aa3bc54
jolijavetzky <riverplate>
2021-06-07 10:55:36 -0300

    Read and write lock for wallet

--- src/main/java/com/sms/challenge/currencywalletapi/persistence/repository/WalletRepository.java
+++ src/main/java/com/sms/challenge/currencywalletapi/persistence/repository/WalletRepository.java
@@ -2,11 +2,36 @@ package com.sms.challenge.currencywalletapi.persistence.repository;
 
 import com.sms.challenge.currencywalletapi.persistence.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.LockModeType;
 
 /**
  * The interface Wallet repository.
  */
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+    /**
+     * Find by id for read wallet.
+     *
+     * @param id the id
+     * @return the wallet
+     */
+    @Lock(LockModeType.PESSIMISTIC_READ)
+    @Query("select w from Wallet w where w.id = :id")
+    Wallet findByIdForRead(@Param("id") Long id);
+
+    /**
+     * Find by id for write wallet.
+     *
+     * @param id the id
+     * @return the wallet
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select w from Wallet w where w.id = :id")
+    Wallet findByIdForWrite(@Param("id") Long id);
 }
